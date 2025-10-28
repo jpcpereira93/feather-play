4bbf7c13361d5d3516e16d14f3a5c4eab0f427a5
jpcpereira93 <jpcpereira93@gmail.com>
2025-10-28 01:31:18 +0000

    feat(core): add spotify API

---
+++ app/core/api/spotify.ts
@@ -0,0 +1,24 @@
+import {
+  AuthorizationCodeWithPKCEStrategy,
+  Scopes,
+  SpotifyApi,
+} from "@spotify/web-api-ts-sdk";
+
+const clientId = import.meta.env.VITE_SPOTIFY_AUTH_CLIENT_ID;
+const redirectUrl = import.meta.env.VITE_SPOTIFY_AUTH_REDIRECT_URL;
+
+const scopes: string[] = [
+  ...Scopes.userDetails,
+  ...Scopes.playlistRead,
+  ...Scopes.userPlaybackRead,
+  ...Scopes.userPlayback,
+  ...Scopes.userPlaybackModify,
+];
+
+const auth = new AuthorizationCodeWithPKCEStrategy(
+  clientId,
+  redirectUrl,
+  scopes,
+);
+
+export const spotifyApi = new SpotifyApi(auth, {});
