960ee853ed3408d38b8221b00fa96cedab53b219
jpcpereira93 <jpcpereira93@gmail.com>
2025-10-29 02:04:12 +0000

    feat(navbar): add navigation

--- app/core/components/IconButton/index.tsx
+++ app/core/components/IconButton/index.tsx
@@ -1,14 +1,23 @@
 import type { ReactNode } from "react";
 
 interface IconButtonProps {
+  ariaLabel?: string;
   children: ReactNode;
+  disabled?: boolean;
   onClick: () => void;
 }
 
-export const IconButton = ({ children, onClick }: IconButtonProps) => (
+export const IconButton = ({
+  ariaLabel,
+  children,
+  disabled,
+  onClick,
+}: IconButtonProps) => (
   <button
+    aria-label={ariaLabel}
+    disabled={disabled}
     className="bg-slate-700/60 text-slate-400 flex justify-center items-center p-2 rounded-lg 
-    hover:cursor-pointer hover:bg-slate-700"
+    hover:cursor-pointer hover:bg-slate-700 disabled:bg-slate-700 disabled:text-slate-500"
     onClick={onClick}
     type="button"
   >
