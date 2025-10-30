d93fb8146c754b1d927e98e27209265166cf9b46
jpcpereira93 <jpcpereira93@gmail.com>
2025-10-30 16:59:39 +0000

    feat(spotify-playable-list): add header bg color based on img

--- app/core/components/SpotifyPlayableList/index.tsx
+++ app/core/components/SpotifyPlayableList/index.tsx
@@ -6,7 +6,7 @@ import type {
   Track,
 } from "@spotify/web-api-ts-sdk";
 import { Play } from "lucide-react";
-import { useCallback } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 import { usePlaySpotifyItemMutation } from "~/core/hooks";
 import { getSpotifyItemImageUrl } from "~/core/utils";
@@ -39,41 +39,69 @@ export const SpotifyPlayableList = ({
 }: SpotifyPlayableListProps) => {
   const { mutate: mutatePlaySpotifyItem } = usePlaySpotifyItemMutation();
 
+  const [backgroundColor, setBackgroundColor] = useState<string>();
+
   const getImgSrc = useCallback(() => getSpotifyItemImageUrl(images), [images]);
 
   const onPlayClick = () => mutatePlaySpotifyItem({ uri });
 
   const onPlayTrack = (uri: string) => mutatePlaySpotifyItem({ uris: [uri] });
 
+  useEffect(() => {
+    const context = document.createElement("canvas").getContext("2d");
+
+    const imgObj = new Image();
+    imgObj.src = getImgSrc();
+    imgObj.setAttribute("crossOrigin", "");
+
+    imgObj.onload = () => {
+      if (context) {
+        context.drawImage(imgObj, 0, 0, 1, 1);
+        const i = context.getImageData(0, 0, 1, 1).data;
+
+        const rgba = `rgba(${i[0]},${i[1]},${i[2]},${i[3]})`;
+
+        setBackgroundColor(rgba);
+      }
+    };
+  }, [getImgSrc]);
+
   return (
-    <div className="h-full w-full flex flex-col pt-4 gap-6">
-      <div className="flex h-40 w-full items-center justify-between px-4">
-        <div className="flex h-full w-4/5 gap-6">
-          <div className="h-40 w-40 rounded-lg overflow-hidden">
-            <img src={getImgSrc()} alt={name}></img>
-          </div>
-          <div className="flex flex-col justify-between  overflow-hidden">
-            <h2 className="capitalize font-semibold">{type}</h2>
-            <div className="flex flex-col gap-1 w-full">
-              <h1 className="text-7xl font-black text-dark-300 truncate overflow-hidden">
-                {name}
-              </h1>
-              <p className="text-sm">{description}</p>
+    <div className="h-full w-full flex flex-col gap-6">
+      <div
+        className="flex h-50 w-full p-4 box-border"
+        style={{
+          background: `linear-gradient(180deg, ${backgroundColor}, transparent)`,
+        }}
+      >
+        <div className="flex h-full w-full items-center justify-between">
+          <div className="flex h-full w-4/5 gap-6">
+            <div className="h-40 w-40 rounded-lg overflow-hidden">
+              <img src={getImgSrc()} alt={name}></img>
             </div>
-            <p className="flex items-center gap-1 text-sm">
-              <span className="font-semibold text-dark-300">{owner}</span>•
-              <span>{tracks.total} tracks</span>
-            </p>
+            <div className="flex flex-col justify-between  overflow-hidden">
+              <h2 className="capitalize font-semibold">{type}</h2>
+              <div className="flex flex-col gap-1 w-full">
+                <h1 className="text-7xl font-black text-dark-300 truncate overflow-hidden">
+                  {name}
+                </h1>
+                <p className="text-sm">{description}</p>
+              </div>
+              <p className="flex items-center gap-1 text-sm">
+                <span className="font-semibold text-dark-300">{owner}</span>•
+                <span>{tracks.total} tracks</span>
+              </p>
+            </div>
           </div>
-        </div>
-        <div className="flex h-full items-center justify-center">
-          <button
-            className="bg-dark-400 text-dark-600 rounded-full p-6 hover:cursor-pointer hover:bg-dark-300"
-            onClick={onPlayClick}
-            type="button"
-          >
-            <Play fill="currentColor" size={40} />
-          </button>
+          <div className="flex h-full items-center justify-center">
+            <button
+              className="bg-dark-400 text-dark-600 rounded-full p-6 hover:cursor-pointer hover:bg-dark-300"
+              onClick={onPlayClick}
+              type="button"
+            >
+              <Play fill="currentColor" size={40} />
+            </button>
+          </div>
         </div>
       </div>
       <SpotifyPlayableListTable
