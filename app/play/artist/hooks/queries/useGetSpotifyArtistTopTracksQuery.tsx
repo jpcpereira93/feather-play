ee238a9961249da5c8d47bd9b553bd4eb2a30bc9
jpcpereira93 <jpcpereira93@gmail.com>
2025-11-02 22:45:05 +0000

    fix(artist): add current user country as market on artist top tracks

--- app/play/artist/hooks/queries/useGetSpotifyArtistTopTracksQuery.tsx
+++ app/play/artist/hooks/queries/useGetSpotifyArtistTopTracksQuery.tsx
@@ -1,12 +1,20 @@
+import type { Market } from "@spotify/web-api-ts-sdk";
 import { useQuery } from "@tanstack/react-query";
 
 import { useSpotifyApiContext } from "~/play/core/context";
+import { useGetCurrentSpotifyUserProfileQuery } from "~/play/core/hooks";
 
 export const useGetSpotifyArtistTopTracksQuery = (artistId: string) => {
   const { spotifyApi } = useSpotifyApiContext();
+  const { data: userProfile } = useGetCurrentSpotifyUserProfileQuery();
 
   return useQuery({
     queryKey: ["spotifyArtistTopTracks", artistId],
-    queryFn: async () => await spotifyApi.artists.topTracks(artistId, "PT"),
+    queryFn: async () =>
+      await spotifyApi.artists.topTracks(
+        artistId,
+        (userProfile?.country as Market) ?? "US",
+      ),
+    enabled: !!userProfile?.country,
   });
 };
