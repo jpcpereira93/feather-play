9218a2fc0031cc56f72a6c2e0f471a56f771de08
jpcpereira93 <jpcpereira93@gmail.com>
2025-11-03 23:50:19 +0000

    build: fix assets path in production builds

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/feather-play/",
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), svgr()],
   server: {
     host: "127.0.0.1", // Needed for local development security issues on spotify auth
