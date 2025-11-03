c31841197d066c3af1614b870285c4deaaed8555
jpcpereira93 <jpcpereira93@gmail.com>
2025-11-03 23:53:03 +0000

    build: fix production build basename

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "@react-router/dev/config";
 
 export default {
+  basename: "/feather-play/",
   ssr: false, // SPA mode, since it'll be deployed to github pages
 } satisfies Config;
